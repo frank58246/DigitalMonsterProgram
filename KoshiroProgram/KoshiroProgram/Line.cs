dd33be1a72172ef87ec09a5f0fe2f88059425993
Frank <Frank@DESKTOP-FA8EJCS>
2021-04-10 09:09:49 +0800

    修正主程式

---
+++ KoshiroProgram/KoshiroProgram/Line.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace KoshiroProgram
+{
+    public class Line
+    {
+        public PointF Start { get; set; }
+        public PointF End { get; set; }
+    }
+}
