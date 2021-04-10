496c13188479e8630a5d5fa8baad03389ef666d5
Frank <frank58246@gmail.com>
2021-04-10 08:32:27 +0800

    新增專案

---
+++ ConsoleApp2/Line.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class Line
+    {
+        public Point Start { get; set; }
+        public Point End { get; set; }
+    }
+}
